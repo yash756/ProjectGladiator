f8ad57fc65c7ced1a008f4ca19e409aef3131109
Yash <penteyash@gmail.com>
2021-04-18 23:04:24 +0530

    first commit

---
+++ schemes-for-farmers/src/main/java/com/lti/SchemesForFarmersApplication.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemesForFarmersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemesForFarmersApplication.class, args);
+	}
+
+}
